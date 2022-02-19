ffd89bf535cfd31b7a20ff5e28cd6c3f6327be45
Paweł Ciejka <pawel.ciejka@robco.pl>
2022-02-19 01:20:08 +0000

    test revert to original

--- test_points3d.cc
+++ test_points3d.cc
@@ -13,46 +13,44 @@ using namespace teaching_project;
 // Place stand-alone function in unnamed namespace.
 namespace {
 void TestPart1() {
-    Points3D<int> a, b;                                     // Two empty Points3D are created.
-    cout << a.size() << " " << b.size() << endl;            // yields 0 0.
-    
+    Points3D<int> a, b;  // Two empty Points3D are created.
+    cout << a.size() << " " << b.size() << endl; // yields 0 0.
     const array<int, 3> a_point2{{7, 10, 11}};
-    Points3D<int> d{a_point2};                              // A Points3D containing (7, 10, 11) should be created.
-    cout << d << std::endl;                                              // Should just print (7, 10, 11).
-    
+    Points3D<int> d{a_point2};  // A Points3D containing (7, 10, 11) should be created.
+    cout << d;  // Should just print (7, 10, 11).
     cout << "Enter a sequence of points (integer)" << endl;
-    cin >> a;                                               
-    cout << "Output1: ";
-    cout << a << std::endl;  // Output should be what user entered.
-    
+    cin >> a; // User enters a set of points in the form:
+              // 3 7 4 -5 3 2 100 1 10 -23
+              // The first 3 specifies number of points. Points are the triples
+              // (7, 4, -5), (3, 2, 100), and (1, 10, -23).
+    cout << "Output1: " << endl;
+    cout << a;  // Output should be what user entered.
     cout << "Enter a sequence of points (integer)" << endl;
     cin >> b; // Enter another sequence.
-    cout << "Output2: ";
-    cout << b << std::endl;
-    
+    cout << "Output2: " << endl;
+    cout << b;
     Points3D<int> c{a};  // Calls copy constructor for c.
     cout << "After copy constructor1 c{a}: " << endl;
-    cout << c << std::endl;;
-    cout << a << std::endl;
-    
+    cout << c;
+    cout << a;
     a = b;  // Should call the copy assignment operator for a.
     cout << "After assignment a = b" << endl;
-    cout << a << std::endl;;
+    cout << a;
     Points3D<int> e = move(c);  // Move constructor for e.
     cout << "After e = move(c) " << endl;
-    cout << e << std::endl;
-    cout << c << std::endl;
+    cout << e;
+    cout << c;
     cout << "After a = move(e) " << endl;
     a = move(e);  // Move assignment operator for a.
-    cout << a << std::endl;
-    cout << e << std::endl;
+    cout << a;
+    cout << e;
 }
 
 void TestPart2() {
     Points3D<double> a, b;
     cout << "Enter a sequence of points (double)" << endl;
     cin >> a; // User provides input for Points3D a.
-    cout << a << std::endl;
+    cout << a;
     cout << "Enter a sequence of points (double)" << endl;
     cin >> b; // User provides input for Points3D b.
     cout << b << endl;
@@ -60,7 +58,7 @@ void TestPart2() {
     cout << a + b << endl;
     Points3D<double> d = a + b;
     cout << "Result of d = a + b" << endl;
-    cout << d << std::endl;
+    cout << d;
     cout << "Second element in a: " << endl;
     cout << a[1][0] << ", " << a[1][1] << ", " << a[1][2] << endl;  // Should print the 2nd element.
 }
@@ -68,7 +66,7 @@ void TestPart2() {
 } // namespace
 
 int main(int argc, char **argv) {
-    TestPart1();
-    TestPart2();
-    return 0;
+  TestPart1();
+  TestPart2();
+  return 0;
 }
